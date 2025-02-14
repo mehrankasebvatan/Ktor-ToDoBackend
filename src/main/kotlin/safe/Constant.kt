2a8a1eadbf124fce2a259074126b70aa1e160bde
Mehran Kasebvatan <mehran.kasebvatan@gmail.com>
2025-02-14 12:32:48 +0330

    changeDatabase

--- src/main/kotlin/safe/Constant.kt
+++ src/main/kotlin/safe/Constant.kt
@@ -3,10 +3,10 @@ package com.example.safe
 import org.flywaydb.core.internal.sqlscript.ParserSqlScript
 
 object Constant {
-    //const val DATABASE_URL = "jdbc:mariadb://localhost:3306/todo_db"
-    //const val USERNAME = "ktor_user"
-    //const val PASSWORD = "ktor_password"
-    const val DATABASE_URL = "mysql://root:fYVGSytNZepIPnB7vYl7kjhH@ktor-todo:3306/confident_hertz"
+    const val DATABASE_URL = "jdbc:mariadb://localhost:3306/todo_db"
+    const val USERNAME = "ktor_user"
+    const val PASSWORD = "ktor_password"
+    /*const val DATABASE_URL = "mysql://root:fYVGSytNZepIPnB7vYl7kjhH@ktor-todo:3306/confident_hertz"
     const val USERNAME = "root"
-    const val PASSWORD = "fYVGSytNZepIPnB7vYl7kjhH"
+    const val PASSWORD = "fYVGSytNZepIPnB7vYl7kjhH"*/
 }
